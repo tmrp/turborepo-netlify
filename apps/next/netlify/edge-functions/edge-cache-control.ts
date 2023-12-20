ce179d9f9b1683b74b2bdc2922246efc2e352002
Timothy Peterson <tim@t2.gg>
2023-12-20 14:22:34 +0100

    initial commit

--- apps/next/netlify/edge-functions/edge-cache-control.ts
+++ apps/next/netlify/edge-functions/edge-cache-control.ts
@@ -9,9 +9,7 @@ export default async function EdgeCacheControl(
   request: Request,
   context: Context
 ) {
-  request.headers.set('Cache-Control', 'public, max-age=0, must-revalidate');
-  request.headers.set(
-    'Netlify-CDN-Cache-Control',
-    'public, s-maxage=31536000, must-revalidate'
-  );
+  request.headers.set('Cache-Control', 'public, max-age=31536000');
+  request.headers.set('Netlify-CDN-Cache-Control', 'public, s-maxage=31536000');
+  request.headers.set('CDN-Cache-Control', 'public, s-maxage=31536000');
 }
